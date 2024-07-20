f30c4aa33afb0eebcc78099a0c6b547dd55abd64
vitorsconzo <105835801+vitorsconzo@users.noreply.github.com>
2024-07-20 04:31:07 +0000

    commit vercel

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
     <div>
         <h1>amor eu te amo demais, vc é perfeita! 😽 😻</h1>
-        <p>vo come seu cuuu</p>
+        <p>fica melhor bb XDDDDDDDD</p>
     </div>
     )
 }
